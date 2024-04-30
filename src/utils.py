f625ab2a3f0b884e7ddacbd341944031361b46df
sam <samuel.j.peter@gmail.com>
2024-04-30 16:56:21 +0200

    bugfix

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,11 @@ logger = Logger(__name__)
 
 
 def combine_list_and_file(list_arg: list[str] = [], file_arg: Path = None) -> list[str]:
+    places = []
     if file_arg:
         try:
             with open(file_arg, 'r') as f:
                 places = f.read().splitlines()
         except FileNotFoundError:
             logger.error(f'File {file_arg} not found.')
-            places = []
     return places + list_arg
